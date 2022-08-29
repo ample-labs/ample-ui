a4eba666810207a8f53a24652d6e5152858867f4
Chemist Darron <darron@ample.im>
2022-08-29 18:03:10 +0900

    chore: fix svgIcon warning

--- src/components/SvgIcon/SvgIcon.tsx
+++ src/components/SvgIcon/SvgIcon.tsx
@@ -1,6 +1,11 @@
 import styled, { DefaultTheme, StyledComponentProps } from 'styled-components';
 
-export type SvgIconProps = StyledComponentProps<'svg', DefaultTheme, {}, never>;
+export type SvgIconProps = StyledComponentProps<
+  'svg',
+  DefaultTheme,
+  Record<string, unknown>,
+  never
+>;
 
 const SvgIcon = styled.svg`
   width: 1em;
